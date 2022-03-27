46adb91f5cb1e84e23d01e34c090a703b8111a97
Atharva Mali <40512661+crazmer@users.noreply.github.com>
2022-03-27 22:35:24 +0530

    Build

---
+++ Assets/Camerarotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerarotation : MonoBehaviour
+{
+    public float speed = 3.5f;
+    private float X;
+    private float Y;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+    }
+}
+
---
+++ Assets/cameracontroller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+    public GameObject[] Camera;
+    public int i = 0;
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            if (i == 4)
+            {
+                ResetCamera();
+
+
+            }
+            nextCamera();
+            i++;
+        }
+    }
+
+    void nextCamera()
+    {
+         if( i < Camera.Length)
+        {
+            
+            
+                Camera[i].SetActive(false);
+
+                Camera[i + 1].SetActive(true);
+            
+            
+
+            Debug.Log(i);
+            
+        }
+        
+    }
+
+    private void ResetCamera()
+    {
+        Camera[0].SetActive(true);
+        Camera[1].SetActive(false);
+        Camera[2].SetActive(false);
+        Camera[3].SetActive(false);
+        i = 0;
+    }
+}
